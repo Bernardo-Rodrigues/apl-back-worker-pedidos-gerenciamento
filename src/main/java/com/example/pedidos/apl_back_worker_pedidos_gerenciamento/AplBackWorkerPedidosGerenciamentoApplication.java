befa762061b205fbc06d0f884d18896f941977e3
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2024-11-19 06:19:00 -0300

    build: init

---
+++ src/main/java/com/example/pedidos/apl_back_worker_pedidos_gerenciamento/AplBackWorkerPedidosGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pedidos.apl_back_worker_pedidos_gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplBackWorkerPedidosGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplBackWorkerPedidosGerenciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pedidos/apl_back_worker_pedidos_gerenciamento/AplBackWorkerPedidosGerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pedidos.apl_back_worker_pedidos_gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplBackWorkerPedidosGerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
