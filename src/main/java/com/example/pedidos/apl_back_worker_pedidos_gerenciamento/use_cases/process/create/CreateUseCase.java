3f8212c0d4065ae84be02e42633f933827a31e49
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2024-11-21 20:55:57 -0300

    feat: producing message for calculated orders and adding correlationId verification using redis

--- src/main/java/com/example/pedidos/apl_back_worker_pedidos_gerenciamento/use_cases/process/create/CreateUseCase.java
+++ src/main/java/com/example/pedidos/apl_back_worker_pedidos_gerenciamento/use_cases/process/create/CreateUseCase.java
@@ -5,6 +5,7 @@ import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.domain.entities
 import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.domain.entities.Product;
 import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.domain.enums.OrderStatus;
 import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.domain.exceptions.ProductNotFoundException;
+import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.domain.producers.OrderProducer;
 import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.domain.repositories.OrderRepository;
 import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.domain.repositories.ProductRepository;
 import com.example.pedidos.apl_back_worker_pedidos_gerenciamento.use_cases.process.create.dto.CreateOrderDTO;
@@ -14,14 +15,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class CreateUseCase {
     private final OrderRepository orderRepository;
+    private final OrderProducer orderProducer;
     private final ProductRepository productRepository;
 
-    public CreateUseCase(OrderRepository orderRepository, ProductRepository productRepository) {
+    public CreateUseCase(OrderRepository orderRepository, OrderProducer orderProducer, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
+        this.orderProducer = orderProducer;
         this.productRepository = productRepository;
     }
 
-    public Order execute(CreateOrderDTO orderDTO) {
+    public void execute(String correlationId, CreateOrderDTO orderDTO) {
         Order order = CreateOrderMapper.toEntity(orderDTO);
 
         for (Item item : order.getItems()) {
@@ -35,6 +38,8 @@ public class CreateUseCase {
         order.calculateTotalValue();
         order.setStatus(OrderStatus.PENDING);
 
-        return orderRepository.save(order);
+        var savedOrder = orderRepository.save(order);
+
+        orderProducer.sendCalculatedOrderMessage(correlationId, savedOrder.getId());
     }
 }
